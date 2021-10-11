28561819655e009767d895b2dc947714ca94bab0
Jalamang <Jalamangjanka@gmail.com>
2021-10-10 23:59:18 -0400

    Added a default image background which is replaced by an background image of the city searched

--- cypress/integration/script.js
+++ cypress/integration/script.js
@@ -1,13 +1,14 @@
 
 
-    
-    document.querySelector('form').addEventListener('submit', (event) => {
-        event.preventDefault();
-        let city = event.target.temperature.value;
-        fetch(`https://wttr.in/${city}?format=j1`)
-            .then((response) => response.json())
-            .then((weather) => {
-                // console.log(weather)
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let city = event.target.temperature.value;
+    fetch(`https://wttr.in/${city}?format=j1`)
+        .then((response) => response.json())
+        .then((weather) => {
+            // console.log(weather)
+            const getMain = () => {
                 document.querySelector('.display p').innerHTML =
                     `<label> <strong> ${weather.nearest_area[0].areaName[0].value}</strong> <label>
                 <div class="current-temp grid-three">
@@ -33,27 +34,39 @@
                 <p><strong>Max Temperature: </strong>${weather.weather[2].mintempF}°F</p>
                 <p><strong>Min Temperature: </strong>${weather.weather[2].maxtempF}°F</p> 
                 </div>`
-
-                
-                    function getCityWeather(loc) {
-                        if (document.querySelector('.history .previous-search').firstElementChild.textContent === 'No previous search') {
-                            document.querySelector('.history .previous-search').firstElementChild.remove()
-                        }
-                        const li = document.createElement("li");
-                        //li.innerHTML += `<a href="https://wttr.in/${loc}" >${loc}</a> - ${weather.current_condition[0].FeelsLikeF} ` ;
-                        li.innerHTML += loc;
-                        document.querySelector('.history .previous-search').append(li);
-                        return li;
-                    }
-                    getCityWeather(city)
-                
-              
-            })
-        event.target.reset();
-    })
+                document.body.style.backgroundImage =
+                    "url('https://source.unsplash.com/1600x900/?" + city + "')";
+            }
+            getMain()
+            function getCityWeather(loc) {
+                if (document.querySelector('.history .previous-search').firstElementChild.textContent === 'No previous search') {
+                    document.querySelector('.history .previous-search').firstElementChild.remove()
+                }
+                const li = document.createElement("li");
+                //li.innerHTML += `<a href="https://wttr.in/${loc}" >${loc}</a> - ${weather.current_condition[0].FeelsLikeF} ` ;
+                li.innerHTML += loc;
+                document.querySelector('.history .previous-search').append(li);
+                return li;
+            }
+            getCityWeather(city)
 
 
+        })
+    event.target.reset();
+})
 
+
+
+const updateCities = () => {
+    const cityLink = document.querySelectorAll('ul li')
+    for (let city of cityLink) {
+        city.addEventListener('click', (event) => {
+            event.preventDefault()
+            //document.querySelectorAll('.history .previous-search li')
+            getMain()
+        })
+    }
+}
 
 
 
