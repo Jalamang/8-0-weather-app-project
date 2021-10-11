b257df023792b3777bfb92a6d0b0c0b172d7a9bc
Jalamang <Jalamangjanka@gmail.com>
2021-10-10 20:21:37 -0400

    Integrated API with JavaScript using fetch

---
+++ cypress/integration/script.js
@@ -0,0 +1,49 @@
+
+
+    
+    document.querySelector('form').addEventListener('submit', (event) => {
+        event.preventDefault();
+        let city = event.target.temperature.value;
+        fetch(`https://wttr.in/${city}?format=j1`)
+            .then((response) => response.json())
+            .then((weather) => {
+                // console.log(weather)
+                document.querySelector('.display p').innerHTML =
+                    `<label> <strong> ${weather.nearest_area[0].areaName[0].value}</strong> <label>
+                <div class="current-temp grid-three">
+                <p><strong> Area:</strong> ${weather.nearest_area[0].areaName[0].value}</p> 
+                <p><strong> Region:</strong> ${weather.nearest_area[0].region[0].value} </p>
+                <p><strong> Country:</strong> ${weather.nearest_area[0].country[0].value}  </p>
+                <p><strong>Currently:</strong> Feels Like ${weather.current_condition[0].FeelsLikeF}°F</p> 
+            </div>`
+
+                document.querySelector('.display .three-day-temp').innerHTML =
+                    ` <div class="today"> <strong>Today</strong>
+                <p><strong>Average Temperature: </strong>${weather.weather[0].avgtempF}°F</p> 
+                <p><strong>Max Temperature: </strong>${weather.weather[0].maxtempF}°F</p>
+                <p><strong>Min Temperature: </strong>${weather.weather[0].mintempF}</p>
+                </div>
+                <div class="tomorrow"><strong>Tomorrow</strong> 
+                <p><strong>Average Temperature: </strong>${weather.weather[1].avgtempF}°F</p> 
+                <p><strong>Max Temperature: </strong> ${weather.weather[1].maxtempF}°F</p>
+                <p><strong>Min Temperature: </strong>${weather.weather[1].mintempF}°F</p> 
+                </div>
+                <div class="day-after"><strong>Day After Tomorrow</strong> 
+                <p><strong>Average Temperature: </strong>${weather.weather[2].avgtempF}°F</p> 
+                <p><strong>Max Temperature: </strong>${weather.weather[2].mintempF}°F</p>
+                <p><strong>Min Temperature: </strong>${weather.weather[2].maxtempF}°F</p> 
+                </div>`
+              
+            })
+        event.target.reset();
+    })
+
+
+
+
+
+
+
+
+
+
