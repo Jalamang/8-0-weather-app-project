dbae743144eadab83b8251aad626384b1ce00837
Jalamang <Jalamangjanka@gmail.com>
2021-10-12 14:19:07 -0400

    Removes duplicates

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ function getCityInfo(city) {
 //Reassigning the textNode/child of second child of the main element, p element 
       document.querySelector(
         ".display .three-day-temp"
-      ).innerHTML = ` <div class="today">Today
+      ).innerHTML = ` <div class="today">Today [${weather.weather[0].date}]
                 <p><strong>Average Temperature: </strong>${weather.weather[0].avgtempF}°F</p> 
                 <p><strong>Max Temperature: </strong>${weather.weather[0].maxtempF}°F</p>
                 <p><strong>Min Temperature: </strong>${weather.weather[0].mintempF}</p>
                 </div>
-                <div class="tomorrow">Tomorrow 
+                <div class="tomorrow">Tomorrow [${weather.weather[1].date}]
                 <p><strong>Average Temperature: </strong>${weather.weather[1].avgtempF}°F</p> 
                 <p><strong>Max Temperature: </strong> ${weather.weather[1].maxtempF}°F</p>
                 <p><strong>Min Temperature: </strong>${weather.weather[1].mintempF}°F</p> 
                 </div>
-                <div class="day-after">Day After Tomorrow 
+                <div class="day-after">Day After Tomorrow [${weather.weather[2].date}]
                 <p><strong>Average Temperature: </strong>${weather.weather[2].avgtempF}°F</p> 
                 <p><strong>Max Temperature: </strong>${weather.weather[2].mintempF}°F</p>
                 <p><strong>Min Temperature: </strong>${weather.weather[2].maxtempF}°F</p> 
@@ -58,16 +58,17 @@ function getCityInfo(city) {
 
 
 function getCityWeather(loc, weather) {
+  
   //Jose M helped in removing the textNode b4 appending an li to the ul
   if (document.querySelector('.history .previous-search').firstElementChild.textContent === 'No previous search') {
     document.querySelector('.history .previous-search').firstElementChild.remove()
   }
-  const removeLiDuplicates = document.querySelectorAll('.history .previous-search')
-// if (!removeLiDuplicates.textContent.includes(loc)) {
+  const removeLiDuplicates = document.querySelector('.grid-two')
+ if (!removeLiDuplicates.textContent.includes(loc.toLowerCase())) {
   const li = document.createElement("li");
   li.innerHTML += `<a href="javascript:getCityInfo('${loc}')">${loc} -${weather.current_condition[0].FeelsLikeF}°F</a>`;
   document.querySelector("ul").append(li);
-// }
+}
 }
   
  
